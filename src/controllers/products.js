6e10214a0277e3b77d93b525d2388c8cdb715097
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-07-21 13:01:28 +0200

    feat: query product

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,17 +1,37 @@
-const getAllProducts = (req, res) => {
-  throw new Error("Testing the async wrapper erros package");
-  res.status(200).json({
-    success: true,
-    status: 200,
-    message: "Retrieving all products",
-    data: "here",
+const Product = require("../models/Product");
+
+const getAllStaticProducts = async (req, res) => {
+  const products = await Product.find({
+    featured: true,
   });
-};
-const getAllStaticProducts = (req, res) => {
   res.status(200).json({
     success: true,
     status: 200,
     message: "Retrieving all static products",
+    nbHits: products.length,
+    products,
+  });
+};
+const getAllProducts = async (req, res) => {
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured == "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  console.log(queryObject);
+  const product = await Product.find(queryObject);
+  res.status(200).json({
+    success: true,
+    status: 200,
+    message: "Retrieving all products",
+    product,
+    nbHits: product.length,
   });
 };
 
