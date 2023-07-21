9ccf11c611c9f37013a3d2b760cb198fac6c6a6a
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-07-21 18:14:54 +0200

    feat: numeric filter

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,12 +1,10 @@
 const Product = require("../models/Product");
 
 const getAllStaticProducts = async (req, res) => {
-  const products = await Product.find({
-    featured: true,
-  })
+  const products = await Product.find({ price: { $gt: 30 } })
     .select("name price")
     .limit(3)
-    .sort("name");
+    .sort("price");
   res.status(200).json({
     success: true,
     status: 200,
@@ -16,7 +14,7 @@ const getAllStaticProducts = async (req, res) => {
   });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, numericFilters } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured == "true" ? true : false;
@@ -32,6 +30,30 @@ const getAllProducts = async (req, res) => {
   const limit = Number(req.query) || 2;
   const skip = (page - 1) * limit;
   result = result.skip(skip).limit(skip);
+
+  console.log(numericFilters);
+  if (numericFilters) {
+    const operatorOBJ = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regex = /\b(<|>|<=|>=|=)\b/g;
+    let filters = numericFilters.replace(
+      regex,
+      (match) => `-${operatorOBJ[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").map((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
+  console.log(queryObject);
   const product = await result;
   res.status(200).json({
     success: true,
