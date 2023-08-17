14e18ca2fd45ed093d29cecc16ba95a0ee19efc5
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-08-17 17:57:36 +0200

    feat: update

--- src/middleware/notFound.js
+++
@@ -1,9 +0,0 @@
-const notFound = (req, res) => {
-  return res.status(404).json({
-    success: false,
-    status: 404,
-    message: `${req.method}=${req.protocol}://${req.hostname}${req.originalUrl}`,
-  });
-};
-
-module.exports = notFound;
