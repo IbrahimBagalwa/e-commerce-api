b05178793a37563f09e33a940eaff9d163a9592b
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-08-21 17:12:53 +0200

    feat: connect db

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express, { Express } from "express";
 import notFoundMiddleware from "./middleware/notFound";
 import errorHandler from "./middleware/errorHandler";
+import dotenv from "dotenv";
+import "express-async-errors";
+dotenv.config();
+
 const app: Express = express();
 
 app.use(express.json());
