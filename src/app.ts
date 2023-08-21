5c74a3a3268223c66815ea3d95bba84fe1584a41
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-08-21 17:54:36 +0200

    feat: connect the db and crate a schema

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
 import notFoundMiddleware from "./middleware/notFound";
 import errorHandler from "./middleware/errorHandler";
 import dotenv from "dotenv";
 import "express-async-errors";
 dotenv.config();
+import morgan from "morgan";
 
 const app: Express = express();
 
+app.use(morgan("tiny"));
 app.use(express.json());
+
+app.get("/", (_req: Request, res: Response) => {
+  return res.send("backend e-commerce api");
+});
+
 app.use(notFoundMiddleware);
 app.use(errorHandler);
 
---
+++ src/models/User.ts
@@ -0,0 +1,33 @@
+import validator from "validator";
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    minLength: 3,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide a valid email address",
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } as any,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minLength: 6,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    deafult: "user",
+  },
+});
+
+export default mongoose.model("User", UserSchema);
