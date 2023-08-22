19d548ef803c557fcee127fcbc8eb658851a5f4b
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-08-22 11:29:58 +0200

    feat: implement the logout functionalities

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -66,6 +66,10 @@ async function login(req: Request, res: Response) {
 }
 
 async function logout(req: Request, res: Response) {
+  res.cookie("token", "logout", {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
   res.status(StatusCodes.OK).json({
     success: true,
     status: StatusCodes.OK,
