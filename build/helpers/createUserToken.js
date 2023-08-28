1061601043e11678660c95cee22078290ce9942b
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-08-28 17:23:28 +0200

    fix: deployment issue

--- build/helpers/createUserToken.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var createUserToken = function (user) {
-    return { username: user.username, userId: user._id, role: user.role };
-};
-exports.default = createUserToken;
